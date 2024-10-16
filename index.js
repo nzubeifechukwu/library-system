f5b8fe691c01b835e39dbe569436803e691f9e8f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-10-16 06:12:04 +0100

    Implement remove-book feature

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ let books = [];
 const addBtn = document.querySelector(".add-btn");
 const displayBtn = document.querySelector(".display-btn");
 const booksContainer = document.querySelector(".books-container");
+const removeBtn = document.querySelector(".remove-btn");
 
 addBtn.addEventListener("click", () => {
-  addBook("title", "author", "yearOfPublication", "genre");
+  addBook();
 });
 
 displayBtn.addEventListener("click", () => {
+  booksContainer.innerHTML = ""; // Make sure container is empty before displaying books
   displayBooks(books);
 });
 
-function addBook(title, author, pubYear, genre) {
+removeBtn.addEventListener("click", () => {
+  removeBook();
+});
+
+function addBook() {
   let book = {};
-  for (let i = 0; i < arguments.length; i++) {
-    book[arguments[i]] = prompt(`Enter book ${arguments[i]}`);
-  }
+  let bookProps = ["title", "author", "yearOfPublication", "genre"];
+  bookProps.forEach((prop) => {
+    book[prop] = prompt(`Enter book ${prop}`);
+  });
   books.push(book);
   console.log(books);
 }
@@ -23,12 +30,24 @@ function addBook(title, author, pubYear, genre) {
 function displayBooks(books) {
   books.forEach((book) => {
     const div = document.createElement("div");
-    // const paragraph = document.createElement("p");
     div.innerHTML = `<p><strong>Title:</strong> ${book["title"]}</p>
       <p><strong>Author:</strong> ${book["author"]}</p>
       <p><strong>Year of Publication:</strong> ${book["yearOfPublication"]}</p>
       <p><strong>Genre:</strong> ${book["genre"]}</p>`;
-    // div.appendChild(divContent);
     booksContainer.appendChild(div);
   });
+}
+
+/**
+ * Remove book by title
+ */
+function removeBook() {
+  const title = prompt("Enter the title of the book you want to remove");
+  books.forEach((book) => {
+    if (book.title.toLowerCase() === title.toLowerCase()) {
+      books.pop(book);
+      // return;
+    }
+  });
+  // alert(`There's no book titled "${title}"`);
 }
